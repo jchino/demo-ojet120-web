36c286b0cab4de067aea2071f56f72d4c6a26bd8
jchino <junko.chino@oracle.com>
2022-07-23 21:36:18 +0900

    minimize appController

--- src/ts/appController.ts
+++ src/ts/appController.ts
@@ -1,24 +1,18 @@
 import Context = require('ojs/ojcontext');
 import CoreRouter = require('ojs/ojcorerouter');
-import KnockoutRouterAdapter = require('ojs/ojknockoutrouteradapter');
 import ModuleRouterAdapter = require('ojs/ojmodulerouter-adapter');
 import UrlParamAdapter = require('ojs/ojurlparamadapter');
 import 'ojs/ojknockout';
 import 'ojs/ojmodule-element';
 
-interface CoreRouterDetail {
-  label: string;
-}
-
 class RootViewModel {
-  moduleAdapter: ModuleRouterAdapter<CoreRouterDetail>;
-  selection: KnockoutRouterAdapter<CoreRouterDetail>;
+  moduleAdapter: ModuleRouterAdapter;
 
   constructor() {
     // Navigation Data
     const navData = [
       { path: '', redirect: 'home'},
-      { path: 'home', detail: { label: 'Home' }},
+      { path: 'home' },
     ];
 
     // Setup Router
@@ -27,7 +21,6 @@ class RootViewModel {
     });
     router.sync();
     this.moduleAdapter = new ModuleRouterAdapter(router);
-    this.selection = new KnockoutRouterAdapter(router);
 
     // Release the application bootstrap busy state
     Context.getPageContext().getBusyContext().applicationBootstrapComplete();
